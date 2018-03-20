362ac633a4c7dccf1daf3e28eac4293f6d9c0cb8
Vojtěch Zicha <vojtech@zicha.name>
2018-03-20 10:43:03 +0100

    FIX: invalid URI addresses in build

--- src/App.js
+++ src/App.js
@@ -77,9 +77,8 @@ class App extends Component {
               path="/onedrive/signin"
               render={() =>
                 (window.location =
-                  'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=a7fba33d-f054-47c3-92d3-27978004647d&scope=files.readwrite.all' +
-                  '&response_type=token&redirect_uri=' +
-                  encodeURIComponent('http://localhost:3000/onedrive/callback'))
+                  `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=a7fba33d-f054-47c3-92d3-27978004647d&scope=files.readwrite.all` +
+                  `&response_type=token&redirect_uri=${encodeURIComponent(`${process.env.PUBLIC_URL}/onedrive/callback`)}`)
               }
             />
             <Route
